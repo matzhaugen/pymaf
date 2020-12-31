75cfad6a24db29695c9b4906c0f751c5f7280104
Matz Haugen <matz.haugen@cognite.com>
2020-12-31 15:57:34 +0100

    change CI

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version="0.2",
     description="Finding Maximum autocorrelation factors",
     url="http://github.com/mahaugen/pymaf",
-    download_url="http://github.com/mahaugen/pymaf/archive/0.2.tar.gz",
     author="Matz Haugen",
     author_email="no@thanks.com",
     license="MIT",
